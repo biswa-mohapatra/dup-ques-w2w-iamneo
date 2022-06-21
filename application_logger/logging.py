b2c8be184b105879f4ad2ef5c1659adc283ae862
Aaric <jeetmohapatra98@gmail.com>
2022-06-21 12:45:55 +0530

    logging.pt: updated by file_object

--- application_logger/logging.py
+++ application_logger/logging.py
@@ -2,11 +2,12 @@ from datetime import datetime
 
 
 class App_Logger:
-    def __init__(self):
+    def __init__(self,file_object):
+        self.file_object = file_object
         pass
 
-    def log(self, file_object, log_message):
+    def log(self,log_message):
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
-        file_object.write(str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message + "\n" )
+        self.file_object.write(str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message + "\n" )
