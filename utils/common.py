1cf30a0bb3308f2b508a5382f657eef69a290292
Aaric <jeetmohapatra98@gmail.com>
2022-06-21 12:46:15 +0530

    common.py created

---
+++ utils/common.py
@@ -0,0 +1,19 @@
+import os
+import yaml
+import logging
+import time
+import pandas as pd
+import json
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    return content
+
+def create_directories(path_to_directories: list) -> None:
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+
+def save_json(path: str, data: dict) -> None:
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
