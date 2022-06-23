bab483b7878b6542c6ed2d7b82cd6ae4b821aeee
Aaric <jeetmohapatra98@gmail.com>
2022-06-23 16:59:21 +0530

    main.py : all the stages are defined

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
-import get_data,data_clean,transform_data
+from src import get_data,data_clean,transform_data,filter_duplicate,duplicate_index
 from utils.common import read_yaml,create_directories
 from application_logger.logging import App_Logger
 
-def main():
+def main(question:str)->str:
     try:
         file_obj =open("MAIN.txt","a+")
         config = read_yaml("config.yaml")
@@ -21,17 +21,47 @@ def main():
         data = data.download()
         log.log(log_message=f"Data getting completed...")
 
-        log.log("Instantiating claning of the data...")
-        data_cleaned = data_clean.clean_data(data)
-        data_cleaned = data_cleaned.remove_tags(drop_null=True)
-        PATH = os.path.join(local_dir,"cleaned_data.csv")
+        PATH = os.path.join(local_dir,f"cleaned_data{school_id}.csv")
         if not os.path.exists(PATH):
+            log.log("Instantiating claning of the data...")
+            data_cleaned = data_clean.clean_data(data)
+            data_cleaned = data_cleaned.remove_tags(drop_null=True,PATH=PATH)
             data_cleaned.to_csv(PATH)
-        log.log(f"Data cleaning completed...")
+            log.log(f"Data cleaning completed...")
+        else:
+            data_cleaned = pd.read_csv(PATH)
+        transform_data_PATH = os.path.join(local_dir,f"transformed_data{school_id}.csv")
+        if not os.path.exists(transform_data_PATH):
+            log.log(f"Starting Data Transformation \n")
+            t = transform_data.transform(data_cleaned)
+            transformed_data = t.transform_data()
+            log.log(f"Data transformation completed...\n")
+            transformed_data.to_csv(transform_data_PATH)
+            log.log(f"Transformed data saved at {transform_data_PATH}")
+        else:
+            transformed_data = pd.read_csv(transform_data_PATH)
+
+        log.log(f"Starting Data Filteration \n")
+        f = filter_duplicate.filter(transformed_data)
+        filtered_data = f.filter_duplicates()
+        log.log(f"Data filteration completed...\n")
+
+        if question:
+            log.log(f"Finding duplicate index started...\n")
+            dup = duplicate_index.duplicate_index(filtered_data,str(question))
+            dup = dup.index()
+            if len(dup)>1:
+                return f"Number of Duplicates found :: {len(dup)-1}"
+            else:
+                return f"Number of duplicates found :: {len(dup)}"
+
 
     except KeyError as k:
         log.log(f"Something went wrong :: {k}\n")
         raise KeyError
+    except AttributeError as a:
+        log.log(f"Something went wrong :: {a}\n")
+        raise AttributeError
     except ValueError as v:
         log.log(f"Something went wrong :: {v}\n")
         raise ValueError
@@ -39,7 +69,9 @@ def main():
         log.log(f"Something went wrong :: {e}\n")
         raise Exception
 
-main()
-
+if __name__ == "__main__":
+    result = main("What is your name?")
+    print(result)
+    #main()
 
     
